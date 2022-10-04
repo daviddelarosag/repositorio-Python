1713736ead61faa7b7fbea5b677ef01f86ccaa48
daviddelarosag <ddasuker@gmail.com>
2022-10-03 22:45:18 -0500

    matriz
    matriz

---
+++ temp.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Matriz bidimensional (10x10) 
+# for anidado
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+# make data
+
+for i in range (1,11):
+    for j in range (1,11):
+        print (f'({i},{j})',end = ' ')
+    print('')
+    
+    plt.show()
+    
+    
+    
+
+
+
+
+    
+    
+    
